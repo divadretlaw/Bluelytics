280eab4310b7c2f7890e1452319c4a9b99cc67c2
David Walter <hello@davidwalter.at>
2023-12-02 22:54:14 +0100

    Improve Tests

--- Tests/BluelyticsTests/BluelyticsTests.swift
+++ Tests/BluelyticsTests/BluelyticsTests.swift
@@ -12,11 +12,18 @@ final class BluelyticsTests: XCTestCase {
     
     func testLatest() async throws {
         let data = try await bluelytics.latest()
-        print(data)
+        
+        let calculatedOfficialAverage = (data.oficial.valueBuy + data.oficial.valueSell) / 2
+        let differenceOfficial = abs(data.oficial.valueAvg - calculatedOfficialAverage)
+        XCTAssertTrue(differenceOfficial < 10)
+        
+        let calculatedBlueAverage = (data.blue.valueBuy + data.blue.valueSell) / 2
+        let differenceBlue = abs(data.blue.valueAvg - calculatedBlueAverage)
+        XCTAssertTrue(differenceBlue < 10)
     }
     
     func testEvolution() async throws {
         let data = try await bluelytics.evolution(days: 7)
-        print(data)
+        XCTAssertEqual(data.count, 7)
     }
 }
